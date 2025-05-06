432dc42845645e3474c72165e8327288af47a1da
Erin Anderson <4420108+eSonOfAnder@users.noreply.github.com>
2025-05-06 14:36:12 -0700

    Add files via upload

---
+++ src/client/java/com/esonofander/abd/AbdClient.java
@@ -0,0 +1,10 @@
+package com.esonofander.abd;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AbdClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
